2a838ce63dcc95af5e281ea0839e203aee91a2a0
Alejo <53284952+aleeperezz16@users.noreply.github.com>
2021-01-17 19:32:12 -0300

    Update CSPlayer.cpp

--- regamedll/dlls/API/CSPlayer.cpp
+++ regamedll/dlls/API/CSPlayer.cpp
@@ -549,14 +549,8 @@ void CCSPlayer::OnSpawn()
 #ifdef REGAMEDLL_ADD
 	if (free_armor.value > 0)
 	{
-		entvars_t* pev = BasePlayer()->pev;
-		pev->armorvalue = 100.0;
-
 		BasePlayer()->m_iKevlar = (int)free_armor.value == 1 ? ARMOR_KEVLAR : ARMOR_VESTHELM;
-
-		MESSAGE_BEGIN(MSG_ONE, gmsgBattery, nullptr, pev);
-		WRITE_SHORT((int)pev->armorvalue);
-		MESSAGE_END();
+		BasePlayer()->pev->armorvalue = 100.0;
 	}
 #endif // REGAMEDLL_ADD
 }
