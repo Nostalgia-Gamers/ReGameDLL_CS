04b102f69a36425b95505febaf86bac814dc5aa0
Alejo <53284952+aleeperezz16@users.noreply.github.com>
2021-01-28 23:31:09 -0300

    Update items.cpp

--- regamedll/dlls/items.cpp
+++ regamedll/dlls/items.cpp
@@ -392,6 +392,13 @@ BOOL CItemKevlar::MyTouch(CBasePlayer *pPlayer)
 		WRITE_STRING(STRING(pev->classname));
 	MESSAGE_END();
 
+#ifndef REGAMEDLL_FIXES
+	MESSAGE_BEGIN(MSG_ONE, gmsgArmorType, nullptr, pPlayer->pev);
+	WRITE_BYTE(pPlayer->m_iKevlar == ARMOR_KEVLAR ? 0 : 1); // 0 = ARMOR_KEVLAR, 1 = ARMOR_VESTHELM
+	WRITE_BYTE(0);
+	MESSAGE_END();
+#endif
+
 	if (TheTutor)
 	{
 		TheTutor->OnEvent(EVENT_PLAYER_BOUGHT_SOMETHING, pPlayer);
@@ -436,6 +443,12 @@ BOOL CItemAssaultSuit::MyTouch(CBasePlayer *pPlayer)
 	MESSAGE_BEGIN(MSG_ONE, gmsgItemPickup, nullptr, pPlayer->pev);
 		WRITE_STRING(STRING(pev->classname));
 	MESSAGE_END();
+
+#ifndef REGAMEDLL_FIXES
+	MESSAGE_BEGIN(MSG_ONE, gmsgArmorType, nullptr, pPlayer->pev);
+	WRITE_BYTE(1); // 0 = ARMOR_KEVLAR, 1 = ARMOR_VESTHELM
+	MESSAGE_END();
+#endif // !REGAMEDLL_FIXES
 
 	if (TheTutor)
 	{
